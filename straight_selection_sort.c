9ca8fcabb3ef542b6435fed2f12ea837d82d9077
omarcartera <c.omargamal@gmail.com>
2019-02-16 14:23:01 +0100

    minor bug fixes

--- straight_selection_sort.c
+++ straight_selection_sort.c
@@ -3,38 +3,42 @@
 
 #include <stdio.h>
 
-#define MAX_NUMBERS 10
+#define MAX_NUMBERS 5
 
 //unsorted set of numbers
-int unsorted[MAX_NUMBERS];
+int numbers[MAX_NUMBERS];
 
 //the set after sorting
 int sorted[MAX_NUMBERS];
 
-//the sorted number position in the new array
-int position=0;
 
 int main()
 {
-    printf("Enter 10 unsorted unique numbers\n");
+    printf("Enter %d unsorted unique numbers\n", MAX_NUMBERS);
 
     //reading the numbers one by one
     for(int i = 0; i < MAX_NUMBERS; i++)
     {
-        scanf(" %d", &unsorted[i]);
+        scanf(" %d", &numbers[i]);
     }
 
     //choosing a pivot as numbers[j] and comparing it to every other number as numbers[k]
     for(int j = 0; j < MAX_NUMBERS; j++)
     {
+    	//the sorted number position in the new array
+		int position = 0;
+
         for(int k = 0; k < MAX_NUMBERS; k++)
         {
-            if(unsorted[j] >= unsorted[k])
-                position++;
+            if(numbers[j] > numbers[k])
+				position++;
         }
 
-        sorted[position-1] = unsorted[j];
-        position = 0;
+        while((numbers[j] == sorted[position]) && (sorted[position] != 0))
+        	position++;
+
+        printf("Position is %d: \n", position);
+        sorted[position] = numbers[j];
     }
 
     //printing the sorted list of numbers
